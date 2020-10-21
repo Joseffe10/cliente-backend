30c137a53c37534b49a6952473cdc8dbf285c8f4
JOSEFFE  Oliveira <OLIVEJ14@sa.ad.whirlpool.com>
2020-10-21 16:17:30 -0300

    Alterações diversas

--- src/config/config.js
+++ src/config/config.js
@@ -1,13 +1,15 @@
+require('dotenv/config');
+
 // Define a string de conexão com o banco de dados
 module.exports = {
     development: {
         database: {
-            host: 'localhost',
-            port: 3306,
-            name: 'db_usuario',
-            dialect: 'mysql',
-            user: 'root',
-            password: 'Joseffe@123'
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            name: process.env.DB_NAME,
+            dialect: process.env.DB_DIALECT,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD
         }
     },
     production: {
