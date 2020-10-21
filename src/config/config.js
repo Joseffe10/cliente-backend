01d486c19bf2afdc95f802c92822f1ee52e7013e
JOSEFFE  Oliveira <OLIVEJ14@sa.ad.whirlpool.com>
2020-10-21 09:25:32 -0300

    Credenciais de BD

--- src/config/config.js
+++ src/config/config.js
@@ -13,7 +13,11 @@ module.exports = {
     production: {
         database: {
             host: process.env.DB_HOST,
-            host: process.env.DB_PORT
+            port: process.env.DB_PORT,
+            name: process.env.DB_NAME,
+            dialect: process.env.DB_DIALECT,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD
         }
     }
 }
