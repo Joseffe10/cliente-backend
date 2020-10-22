6b0a30ff25fa1c937d9238a504f8adee69cd1840
JOSEFFE  Oliveira <OLIVEJ14@sa.ad.whirlpool.com>
2020-10-21 21:22:53 -0300

    Validação de nome

--- src/models/usuario.js
+++ src/models/usuario.js
@@ -16,7 +16,7 @@ const Usuario = sequelize.define("usuario", {
         allowNull: false,
         type: Sequelize.STRING(100),
         validate: {
-            len: [3, 100]
+            isEmail: true
         }
     },
     salario: {
